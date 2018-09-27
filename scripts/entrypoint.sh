b650e24dcd2343b2c363a4cf9d2aa1788f03a566
Matt Stobo <mstobo@500px.com>
2018-09-27 13:20:09 -0400

    Decrypt KMS before anything else

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+eval $(kms-env decrypt)
+
 if [[ -n $DATADOG_ENABLE ]]; then
     envsubst < /opt/templates/datadog.conf.tmpl > /etc/dd-agent/datadog.conf
     mkdir -p /var/log/datadog
     chown dd-agent:dd-agent /var/log/datadog
     /etc/init.d/datadog-agent start
 fi
-
-eval $(kms-env decrypt)
 
 exec "$@"
